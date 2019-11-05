ef347e8afc1396d711cda84e99c9dd1a28d09289
NathanEscandor <nathan.escandor@gmail.com>
2019-11-05 16:48:53 -0700

    Made test directory and skelly for nuc test

---
+++ test/nucleotide-test.js
@@ -0,0 +1,16 @@
+'use strict';
+const codon = require('central-dogma');
+const assert = require('assert');
+
+describe('Nucleotide Functions Test', () => {
+
+  //declare variables here
+  beforeEach(() => {
+
+  });
+
+  it('should do a thing', done => {
+
+  });
+
+});
